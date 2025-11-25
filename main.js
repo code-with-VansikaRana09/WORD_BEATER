74ee8196c8fe170893ca307ee0e84cbb2b808713
code-with-VansikaRana09 <vansika219btcse23@igdtuw.ac.in>
2025-11-25 15:28:20 +0530

    Update main.js

--- main.js
+++ main.js
@@ -70,7 +70,7 @@ if(time>0){
      
 }
 if(time==0) message.textContent="!!GAME OVER!!"
-
+score=0
     }
 function init(){
 // console.log("Hello")
@@ -83,3 +83,4 @@ setInterval(countTime,1000)
     // })
 
     window.addEventListener("DOMContentLoaded",init)
+
