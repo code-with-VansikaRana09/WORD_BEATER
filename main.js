4d7eff741ea9dd75e9f301d80a907aee28533c32
code-with-VansikaRana09 <vansika219btcse23@igdtuw.ac.in>
2025-11-25 15:37:45 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,86 +1,93 @@
-//alert("hi")
 // Array of Words
 const words = [
-  'Cynosure',
-  'Ephemeral',
-  'Quixotic',
-  'Serendipity',
-  'Anachronistic',
-  'Labyrinthine',
-  'Petrichor',
-  'Abstentious',
-  'Sonorous',
-  'Limerence',
-  'Accoutrements',
-  'Halcyon',
-  'Obfuscate',
-  'Soliloquy',
-  'Zephyr',
-  'Reverie',
-  'Ethereal',
-  'Chiaroscuro',
-  'Susurrus',
-  'Mellifluous',
-  'Lugubrious',
-  'Epiphany',
-  'Vellichor',
-  'Umbra',
-  'Aesthete'
-];
+    'hat',
+    'river',
+    'lucky',
+    'statue',
+    'stubborn',
+    'cocktail',
+    'runaway',
+    'joke',
+    'developer',
+    'establishment',
+    'hero',
+    'javascript',
+    'nutrition',
+    'revolver',
+    'echo',
+    'siblings',
+    'investigate',
+    'horrendous',
+    'symptom',
+    'laughter',
+    'magic',
+    'master',
+    'space',
+    'defintion'
+    ];
 
-    let score=0
-    let time=6
-    let currentWord=document.querySelector("#current-word")
-    // console.log(currentWord)
-    let wordInput = document.querySelector("#word-input")
-    //console.log(wordInput)
-    let scoreDisplay = document.querySelector("#score")
-    //console.log(scoreDisplay)
-    let timeDisplay = document.querySelector("#time")
-    let message = document.querySelector("#message")
-    function showWords(words){
-        const randIndex= Math.floor(Math.random()*words.length)
-    //console.log(words[randIndex])
-    currentWord.textContent=words[randIndex]
-    }
-    function matchWord(){
-        if(wordInput.value==currentWord.textContent){
-           
-            return true;
-        }else{
-            return false;
-        }
-    }
-    function startMatch(){
-        if(matchWord()){
-            showWords(words)
-            wordInput.value=''
-            score++
-            scoreDisplay.textContent=score
-            time=6
-            message.textContent="CORRECT!!"
-        }else{
-            message.textContent="WRONG!!"
-        }
-    }
-    function countTime(){
-if(time>0){
-   time--
-    timeDisplay.textContent=time
-     
+let score = 0
+let time = 6
+
+
+let currentWord = document.querySelector("#current-word")
+let wordInput = document.querySelector("#word-input")
+let scoreDisplay = document.querySelector("#score")
+let timeDisplay = document.querySelector("#time")
+let message = document.querySelector("#message")
+
+// console.log(timeDisplay)
+// console.log(scoreDisplay)
+// console.log(wordInput)
+
+
+function showWords(words){
+    const randIndex = Math.floor(Math.random()*words.length) // 23
+    // console.log(words[randIndex])
+    currentWord.textContent = words[randIndex]
 }
-if(time==0) message.textContent="!!GAME OVER!!"
-score=0
+
+function matchWord(){
+        if(wordInput.value == currentWord.textContent){
+            return true
+        }
+        else
+    return false
+}
+
+function startMatch(){
+    if(matchWord()){
+        showWords(words)
+        wordInput.value = ''
+        score++
+        scoreDisplay.textContent = score
+        time = 6
+        message.textContent = "Correct"
+    }else{
+    message.textContent = "InCorrect"
     }
+}
+
+function countdown(){
+    if(time>0) time--
+    timeDisplay.textContent = time
+    if(time==0)  {
+        message.textContent = "Game Over!!!"
+        score = 0
+        scoreDisplay.textContent = score
+    }
+
+
+}
+
 function init(){
-// console.log("Hello")
-showWords(words)
-wordInput.addEventListener("input",startMatch)
-setInterval(countTime,1000)
-}
-    // window.addEventListener('DOMContentLoaded',function(){
-    //     init()
-    // })
+    showWords(words)//get a Random word on the page from the array
+    wordInput.addEventListener("input",startMatch) // when we input an anything on the input box startMatch function call
+    setInterval(countdown,1000)
+}   
 
-    window.addEventListener("DOMContentLoaded",init)
+// window.addEventListener('DOMContentLoaded',function(){
+//     init()
+// })
+window.addEventListener("DOMContentLoaded",init)
 
